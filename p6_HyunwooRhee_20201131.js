8cb058cb269b459f615af79354d59e2d7b622851
IENGROUND <ericano.rhee@gmail.com>
2021-04-29 18:19:25 +0900

    210429_p6_TypoLogo Final Commit

--- p6_HyunwooRhee_20201131.js
+++ p6_HyunwooRhee_20201131.js
@@ -4,9 +4,16 @@
  * Ericano Rhee on github.com/ienground
  */
 
-let stage = 0;
-let currentColor;
+let backgroundColor, foregroundColor;
 let button, input;
+let bgSliderR, bgSliderG, bgSliderB;
+let fgSliderR, fgSliderG, fgSliderB;
+let content = 'IENGROUND';
+let count = 0, countDirection = 1;
+let size = 50;
+
+// checkbox
+let isSquare, isColorRandom;
 
 function preload() {
     mainFont = loadFont('src/ProductSans-Regular.ttf');
@@ -17,51 +24,132 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background('#2B2B2B');
-    currentColor = color('#000000');
     noStroke();
     angleMode(DEGREES);
 
-    input = createInput();
+    input = createInput("IENGROUND");
     button = createButton("SET");
-    input.position(50, height - 75);
-    button.position(input.x + input.width, input.y);
+    button.position(width - 25 - button.width, height - 65 - button.height / 2);
+    input.position(button.x - input.width, height - 65 - button.height / 2);
+    button.mousePressed(setTypoLogo);
 
+    fill(255);
+    textAlign(LEFT, TOP);
+    // text("Background", 110, height - 100);
 
+    bgSliderR = createSlider(0, 255, 0);
+    bgSliderG = createSlider(0, 255, 0);
+    bgSliderB = createSlider(0, 255, 0);
+    fgSliderR = createSlider(0, 255, 255);
+    fgSliderG = createSlider(0, 255, 255);
+    fgSliderB = createSlider(0, 255, 255);
+
+    isSquare = createCheckbox("set logo square", true);
+    isColorRandom = createCheckbox("set color animated", false);
+
+    text("BG", 110, height - 100);
+    text("R", 140, height - 100);
+    text("G", 140, height - 70);
+    text("B", 140, height - 40);
+
+    bgSliderR.position(155, height - 100);
+    bgSliderG.position(155, height - 70);
+    bgSliderB.position(155, height - 40);
+
+    text("FG", bgSliderR.x + bgSliderR.width + 20, height - 100);
+    text("R", bgSliderR.x + bgSliderR.width + 50, height - 100);
+    text("G", bgSliderR.x + bgSliderR.width + 50, height - 70);
+    text("B", bgSliderR.x + bgSliderR.width + 50, height - 40);
+
+    fgSliderR.position(bgSliderR.x + bgSliderR.width + 70, height - 100);
+    fgSliderG.position(bgSliderR.x + bgSliderR.width + 70, height - 70);
+    fgSliderB.position(bgSliderR.x + bgSliderR.width + 70, height - 40);
+
+    isSquare.style('color', '#FFFFFF');
+    isColorRandom.style('color', '#FFFFFF');
+    isSquare.position(fgSliderR.x + fgSliderR.width + 20, height - 65 - button.height * 1.5);
+    isColorRandom.position(fgSliderR.x + fgSliderR.width + 20, height - 65 + button.height * 0.5);
+
+    backgroundColor = color('#000000');
+    foregroundColor = color('#FFFFFF');
 }
 
 function draw() {
-
-    stroke('#FFFFFF');
-    strokeWeight(5);
-    line(50, height - 100, width - 50, height - 100);
-
+    background('#2B2B2B');
+    if (count >= 255) {
+        countDirection = -1;
+    } else if (count <= 0) {
+        countDirection = 1;
+    }
     noStroke();
-    fill(currentColor);
+    count += countDirection;
 
-    let size = 100;
-    let c1 = color('#FF0000');
-    let c2 = color('#c17d7d');
+    backgroundColor = color(bgSliderR.value(), bgSliderG.value(), bgSliderB.value());
+    foregroundColor = color(fgSliderR.value(), fgSliderG.value(), fgSliderB.value());
 
-    let t = ".";
-    // let text = "f";
-    let row = Math.ceil(sqrt(t.length));
-    let column = Math.ceil(t.length / row);
+    let row = Math.ceil(sqrt(content.length));
 
-    fill(255);
-    text(row, 20, 20);
-    text(column, 20, 60);
-    // for (let i = 0; i < t.length; i++) {
-    //     getLetterPattern(t[i], size, c1, c2, width / 2 + (i - (t.length - 1) / 2) * size, height / 2);
-    // }
+    if (isSquare.checked()) {
+        colorMode(RGB, row - 1, row - 1, 255);
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < row; j++) {
+                if (j + i * row < content.length) {
+                    if (isColorRandom.checked()) {
+                        let bgColor = color(i, j, count);
+                        let fgColor = color((i + 1) % row, (j + 1) % row, count);
+                        getLetterPattern(content[j + i * row], size, bgColor, fgColor, width / 2 + (j - (row - 1) / 2) * size, height / 2 - 62.5 + (i - (row - 1) / 2) * size);
+                    } else {
+                        getLetterPattern(content[j + i * row], size, backgroundColor, foregroundColor, width / 2 + (j - (row - 1) / 2) * size, height / 2 - 62.5 + (i - (row - 1) / 2) * size);
+                    }
+                } else {
+                    if (isColorRandom.checked()) {
+                        let bgColor = color(j, i, count);
+                        let fgColor = color(i, j, count);
+                        getLetterPattern("", size, bgColor, fgColor, width / 2 + (j - (row - 1) / 2) * size, height / 2 - 62.5 + (i - (row - 1) / 2) * size);
+                    } else {
+                        getLetterPattern("", size, backgroundColor, foregroundColor, width / 2 + (j - (row - 1) / 2) * size, height / 2 - 62.5 + (i - (row - 1) / 2) * size);
+                    }
 
-    for (let i = 0; i < column; i++) {
-        for (let j = 0; j < row; j++) {
-            if (j + i * row < t.length) {
-                getLetterPattern(t[j + i * row], size, c1, c2, width / 2 + (j - (row - 1) / 2) * size, height / 2 + (i - (column - 1) / 2) * size);
+                }
             }
+        }
+        colorMode(RGB, 255);
+    } else {
+        colorMode(RGB, content.length, 255, 255);
+        for (let i = 0; i < content.length; i++) {
+            if (isColorRandom.checked()) {
+                let bgColor = color(i, 128, count);
+                let fgColor = color((i + content.length / 2) % content.length, 128, count);
+                getLetterPattern(content[i], size, bgColor, fgColor, width / 2 + (i - (content.length - 1) / 2) * size, height / 2 - 62.5);
+            } else {
+                getLetterPattern(content[i], size, backgroundColor, foregroundColor, width / 2 + (i - (content.length - 1) / 2) * size, height / 2 - 62.5);
+            }
+
         }
     }
 
+    fill('#2B2B2B');
+    rect(0, height - 125, width, height);
+    stroke('#FFFFFF');
+    strokeWeight(5);
+    line(25, height - 125, width - 25, height - 125);
+    noStroke();
+
+    fill(255);
+    text("BG", 110, height - 100);
+    text("R", 140, height - 100);
+    text("G", 140, height - 70);
+    text("B", 140, height - 40);
+
+    text("FG", bgSliderR.x + bgSliderR.width + 20, height - 100);
+    text("R", bgSliderR.x + bgSliderR.width + 50, height - 100);
+    text("G", bgSliderR.x + bgSliderR.width + 50, height - 70);
+    text("B", bgSliderR.x + bgSliderR.width + 50, height - 40);
+
+    fill(backgroundColor);
+    square(25, height - 100, 75);
+    fill(foregroundColor);
+    square(40, height - 85, 45);
 }
 
 function setTitle(title) {
@@ -73,8 +161,16 @@ function setTitle(title) {
     text(title, 50, 50);
 }
 
-function mouseClicked() {
-    currentColor = get(mouseX, mouseY)
+function setTypoLogo() {
+    content = input.value();
+}
+
+function mouseWheel(event) {
+    if (event.delta >= 0 && size < 150) {
+        size++;
+    } else if (event.delta < 0 && size > 0) {
+        size--;
+    }
 }
 
 function getLetterPattern(l, size, c1, c2, x, y) {
@@ -83,41 +179,44 @@ function getLetterPattern(l, size, c1, c2, x, y) {
     square(-size / 2, -size / 2, size);
     fill(c2);
     switch (l) {
+        case 'A':
         case 'a': {
             triangle(0, -size / 2, -size / 2, size / 2, size / 2, size / 2);
             break;
         }
-
+        case 'B':
         case 'b': {
             rect(-size / 2, -size / 2, size / 2, size);
             circle(size / 4, -size / 4, size / 2);
             circle(size / 4, size / 4, size / 2);
             break;
         }
-
+        case 'C':
         case 'c': {
             arc(0, 0, size, size, 90, 270, PIE);
             triangle(0, -size / 2, 0, 0, size / 2, -size / 2);
             triangle(0, size / 2, 0, 0, size / 2, size / 2);
             break;
         }
-
+        case 'D':
         case 'd': {
             rect(-size / 2, -size / 2, size / 2, size);
             arc(0, 0, size, size, 270, 90, PIE);
             break;
         }
-
+        case 'E':
         case 'e': {
             arc(0, 0, size, size, 90, 360, PIE);
             triangle(0, 0, 0, size / 2, size / 2, size / 2);
             break;
         }
+        case 'F':
         case 'f': {
             quad(-size / 2, -size / 2, size / 2, -size / 2, 0, 0, -size / 2, 0);
             quad(-size / 2, 0, size / 2, 0, 0, size / 2, -size / 2, size / 2);
             break;
         }
+        case 'G':
         case 'g': {
             fill(c1);
             square(-size / 2, -size / 2, size);
@@ -127,6 +226,7 @@ function getLetterPattern(l, size, c1, c2, x, y) {
             circle(size / 4, size / 4, size / 2);
             break;
         }
+        case 'H':
         case 'h': {
             fill(c2);
             square(-size / 2, -size / 2, size);
@@ -135,6 +235,7 @@ function getLetterPattern(l, size, c1, c2, x, y) {
             arc(0, size / 2, size, size, 180, 0, PIE);
             break;
         }
+        case 'I':
         case 'i': {
             fill(c2);
             square(-size / 2, -size / 2, size);
@@ -143,53 +244,63 @@ function getLetterPattern(l, size, c1, c2, x, y) {
             arc(-size / 2, 0, size, size, 270, 90, PIE);
             break;
         }
+        case 'J':
         case 'j': {
             square(0, -size / 2, size / 2);
             arc(0, 0, size, size, 0, 180, PIE);
             break;
         }
+        case 'K':
         case 'k': {
             rect(-size / 2, -size / 2, size / 2, size);
             triangle(0, -size / 2, 0, 0, size / 2, -size / 2);
             triangle(0, size / 2, 0, 0, size / 2, size / 2);
             break;
         }
+        case 'L':
         case 'l': {
             rect(-size / 2, -size / 2, size / 2, size);
             rect(-size / 2, 0, size, size / 2);
             break;
         }
+        case 'M':
         case 'm': {
             rect(-size / 2, -size / 2, size, size / 2);
             triangle(-size / 2, 0, -size / 2, size / 2, 0, 0);
             triangle(size / 2, 0, size / 2, size / 2, 0, 0);
             break;
         }
+        case 'N':
         case 'n': {
             quad(-size / 2, -size / 2, 0, 0, 0, size / 2, -size / 2, size / 2);
             quad(size / 2, size / 2, 0, 0, 0, -size / 2, size / 2, -size / 2);
             break;
         }
+        case 'O':
         case 'o': {
             circle(0, 0, size);
             break;
         }
+        case 'P':
         case 'p': {
             rect(-size / 2, -size / 2, size / 2, size);
             rect(0, -size / 2, size / 4, size / 2);
             circle(size / 4, -size / 4, size / 2);
             break;
         }
+        case 'Q':
         case 'q': {
             circle(0, 0, size);
             triangle(0, 0, size / 2, 0, size / 2, size / 2);
             break;
         }
+        case 'R':
         case 'r': {
             rect(-size / 2, -size / 2, size / 2, size);
             circle(size / 4, -size / 4, size / 2);
             break;
         }
+        case 'S':
         case 's': {
             arc(-size / 4, -size / 4, size / 2, size / 2, 90, 270, PIE);
             arc(size / 4, size / 4, size / 2, size / 2, 270, 90, PIE);
@@ -197,39 +308,54 @@ function getLetterPattern(l, size, c1, c2, x, y) {
             rect(-size / 2, 0, size * 3 / 4, size / 2);
             break;
         }
+        case 'T':
         case 't': {
             rect(-size / 2, -size / 2, size, size / 2);
             square(-size / 4, 0, size / 2);
             break;
         }
+        case 'U':
         case 'u': {
             rect(-size / 2, -size / 2, size, size / 2);
             arc(0, 0, size, size, 0, 180, PIE);
             break;
         }
+        case 'V':
         case 'v': {
             triangle(-size / 2, -size / 2, size / 2, -size / 2, 0, size / 2);
             break;
         }
+        case 'W':
         case 'w': {
             triangle(-size / 2, -size / 2, 0, -size / 2, 0, size / 2);
             triangle(0, -size / 2, size / 2, -size / 2, size / 2, size / 2);
             break;
         }
+        case 'X':
         case 'x': {
             triangle(-size / 2, -size / 2, 0, 0, -size / 2, size / 2);
             triangle(size / 2, -size / 2, 0, 0, size / 2, size / 2);
             break;
         }
+        case 'Y':
         case 'y': {
             triangle(-size / 2, -size / 2, 0, 0, -size / 2, 0);
             triangle(size / 2, -size / 2, 0, 0, size / 2, 0);
             quad(0, 0, size / 2, 0, size / 2, size / 2, -size / 2, size / 2);
             break;
         }
+        case 'Z':
         case 'z': {
             quad(-size / 2, -size / 2, size / 2, -size / 2, 0, 0, -size / 2, 0);
             quad(0, 0, size / 2, 0, size / 2, size / 2, -size / 2, size / 2);
+            break;
+        }
+        case '.': {
+            square(0, 0, size / 2);
+            break;
+        }
+        case ',': {
+            triangle(0, 0, size / 2, 0, size / 2, size / 2);
             break;
         }
     }
